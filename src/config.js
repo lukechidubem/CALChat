475c0737d63a53958450748cefd9dc9a34872ba6
lukechidubem <lukechidubem@gmail.com>
2023-04-08 10:49:35 +0100

    added backend base url

--- src/config.js
+++ src/config.js
@@ -4,7 +4,8 @@ import { enUS, frFR, zhCN, viVN, arSD } from "@mui/material/locale";
 // routes
 import { PATH_DASHBOARD } from "./routes/paths";
 
-export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://calchat-api.onrender.com";
 
 export const defaultSettings = {
   themeMode: "light",
