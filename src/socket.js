636f7de0f58a4679f4e5ca8352f62f36f515a690
lukechidubem <lukechidubem@gmail.com>
2023-04-08 10:31:19 +0100

    Added backend url

--- src/socket.js
+++ src/socket.js
@@ -2,8 +2,11 @@ import io from "socket.io-client";
 
 let socket;
 
+// https://calchat-api.onrender.com
+
 const connectSocket = (user_id) => {
-  socket = io.connect("http://localhost:5000", {
+  // socket = io.connect("http://localhost:5000", {
+  socket = io.connect("https://calchat-api.onrender.com", {
     query: `user_id=${user_id}`,
     // query: `user_id=${window.localStorage.getItem("user_id")}`,
   }); // Add this -- our server will run on port 5000, so we connect to it from here
